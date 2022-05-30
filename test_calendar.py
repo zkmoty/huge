7f70246a60061bbf9948bea259659180e5e01e99
Udi Oron <udioron@gmail.com>
2022-05-30 13:47:56 +0300

    added another day test

--- test_calendar.py
+++ test_calendar.py
@@ -6,6 +6,7 @@ def test_june_2022():
     s = o.formatmonth(2022, 6)
     assert "June 2022" in s
     assert s.splitlines()[-1] == "27 28 29 30"
+    assert s.split()[-30:] == [str(x) for x in range(1, 31)]
 
 
 def test_july_2022():
