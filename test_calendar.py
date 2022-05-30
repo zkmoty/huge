7e59a88420161d0c709c8afd9df0396f70180176
Udi Oron <udioron@gmail.com>
2022-05-30 13:24:12 +0300

    extracted deck varibale

--- test_calendar.py
+++ test_calendar.py
@@ -10,4 +10,5 @@ def test_june_2022():
 
 
 def test_product():
-    assert len(list(itertools.product("♠♥♦♣", range(1, 14)))) == 52
+    deck = list(itertools.product("♠♥♦♣", range(1, 14)))
+    assert len(deck) == 52
